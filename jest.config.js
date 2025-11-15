c4198048121d9f1860c420a8486faec4811ca39c
Kacper <kacperlachowiczwp.pl@wp.pl>
2025-11-15 19:30:09 +0100

    test: configure Jest for unit testing
    - Set up Jest with ts-jest for TypeScript support
    - Configure test coverage thresholds
    - Set up test file patterns and environment

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/*.spec.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        experimentalDecorators: true,
+        emitDecoratorMetadata: true,
+      },
+    }],
+  },
+  coverageDirectory: './coverage',
+  testTimeout: 10000,
+};
+
